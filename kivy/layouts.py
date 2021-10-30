ce0f5fd53fadeb1ad85b289725e749e7316f8689
Bade-Rohan <077bei036.rohan@pcampus.edu.np>
2021-10-30 17:08:53 +0545

    py file with grid code to show 9 buttons in grid layout

--- kivy/layouts.py
+++ kivy/layouts.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 class BoxLayoutExample(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -25,6 +27,18 @@ class BoxLayoutExample(BoxLayout):
         b2.font_name ="../fonts/Lcd.ttf"
         self.add_widget(b2)
 
+class GridLayoutExample(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rows =3
+        self.cols =3
+        self.spacing ="20dp"
+        b = []
+        for i in range(1,10):
+            b = Button(text=str(i))
+            b.font_name = "../fonts/Lcd.ttf"
+            b.font_size = 50
+            self.add_widget(b)
 
 class LayoutApp(App):
     pass
